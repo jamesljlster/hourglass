1eac74831adbaf240723998a65a5a8f4ed57070f
Zheng-Ling Lai <jamesljlster@gmail.com>
2017-11-26 17:15:15 +0800

    Using data manage structure

--- lib/trasvc/trasvc.c
+++ lib/trasvc/trasvc.c
@@ -3,30 +3,99 @@
 
 #include "trasvc.h"
 #include "trasvc_private.h"
-#include "trasvc_types.h"
 
 #include "debug.h"
 
-int trasvc_create(trasvc_t* svcPtr, lstm_config_t lstmCfg, int dataLimit)
+int trasvc_data_struct_init(struct TRASVC_DATA* dataPtr, int rowLimit, int cols)
 {
 	int i;
-	int rows, cols;
+	int rows;
+	int ret = TRASVC_NO_ERROR;
+
+	LOG("enter");
+
+	// Set rows
+	rows = rowLimit + 1;
+
+	// Memory allocation
+	trasvc_alloc(dataPtr->data, rows, float*, ret, ERR);
+	dataPtr->dataMemLen = rows;
+	for(i = 0; i < rows; i++)
+	{
+		trasvc_alloc(dataPtr->data[i], cols, float, ret, ERR);
+	}
+
+	// Create mutex and condition variable
+	ret = pthread_mutex_init(&dataPtr->mutex, NULL);
+	if(ret < 0)
+	{
+		ret = TRASVC_SYS_FAILED;
+		goto ERR;
+	}
+	else
+	{
+		dataPtr->mutexStatus = 1;
+	}
+
+	ret = pthread_cond_init(&dataPtr->cond, NULL);
+	if(ret < 0)
+	{
+		ret = TRASVC_SYS_FAILED;
+		goto ERR;
+	}
+	else
+	{
+		dataPtr->condStatus = 1;
+	}
+
+	goto RET;
+
+ERR:
+	trasvc_data_struct_cleanup(dataPtr);
+
+RET:
+	LOG("exit");
+	return ret;
+}
+
+void trasvc_data_struct_cleanup(struct TRASVC_DATA* dataPtr)
+{
+	int i;
+
+	LOG("enter");
+
+	if(dataPtr->data != NULL)
+	{
+		for(i = 0; i < dataPtr->dataMemLen; i++)
+		{
+			trasvc_free(dataPtr->data[i]);
+		}
+		trasvc_free(dataPtr->data);
+	}
+
+	if(dataPtr->mutexStatus > 0)
+	{
+		pthread_mutex_destroy(&dataPtr->mutex);
+	}
+
+	if(dataPtr->condStatus > 0)
+	{
+		pthread_cond_destroy(&dataPtr->cond);
+	}
+
+	LOG("exit");
+}
+
+int trasvc_create(trasvc_t* svcPtr, lstm_config_t lstmCfg, int dataLimit)
+{
+	int cols;
 	int ret = TRASVC_NO_ERROR;
 	struct TRASVC* tmpSvcPtr = NULL;
 
 	LOG("enter");
 
-	// Find data rows, cols
-	rows = dataLimit + 1;
-	cols = lstm_config_get_inputs(lstmCfg) + lstm_config_get_outputs(lstmCfg);
-
 	// Memory allocation
 	trasvc_alloc(tmpSvcPtr, 1, struct TRASVC, ret, RET);
-	trasvc_alloc(tmpSvcPtr->data, rows, float*, ret, ERR);
-	for(i = 0; i < rows; i++)
-	{
-		trasvc_alloc(tmpSvcPtr->data[i], cols, float, ret, ERR);
-	}
 
 	// Clone lstm config
 	trasvc_run(lstm_config_clone(&tmpSvcPtr->lstmCfg, lstmCfg), ret, ERR);
@@ -34,8 +103,12 @@ int trasvc_create(trasvc_t* svcPtr, lstm_config_t lstmCfg, int dataLimit)
 	// Create lstm state
 	trasvc_run(lstm_state_create(&tmpSvcPtr->lstmState, lstmCfg), ret, ERR);
 
+	// Create data pool
+	cols = lstm_config_get_inputs(lstmCfg) + lstm_config_get_outputs(lstmCfg);
+	trasvc_run(trasvc_data_struct_init(&tmpSvcPtr->traData, dataLimit, cols), ret, ERR);
+	trasvc_run(trasvc_data_struct_init(&tmpSvcPtr->mgrData, dataLimit, cols), ret, ERR);
+
 	// Assign value
-	tmpSvcPtr->dataMemLen = rows;
 	*svcPtr = tmpSvcPtr;
 
 	goto RET;
@@ -50,26 +123,27 @@ RET:
 
 void trasvc_delete(trasvc_t svc)
 {
-	int i;
+	LOG("enter");
 
 	if(svc != NULL)
 	{
+		if(svc->traTaskStatus > 0)
+		{
+			pthread_cancel(svc->traTask);
+			pthread_join(svc->traTask, NULL);
+		}
+
+		trasvc_data_struct_cleanup(&svc->traData);
+		trasvc_data_struct_cleanup(&svc->mgrData);
+
 		lstm_delete(svc->lstm);
 		lstm_state_delete(svc->lstmState);
 		lstm_config_delete(svc->lstmCfg);
 
-		if(svc->data != NULL)
-		{
-			for(i = 0; i < svc->dataMemLen; i++)
-			{
-				trasvc_free(svc->data[i]);
-			}
-
-			trasvc_free(svc->data);
-		}
-
 		trasvc_free(svc);
 	}
+
+	LOG("exit");
 }
 
 int trasvc_start(trasvc_t svc);
