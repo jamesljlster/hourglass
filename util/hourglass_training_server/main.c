282dea8bb3741ca504fa5a6fe985ff2ee709bb3e
Zheng-Ling Lai <jamesljlster@gmail.com>
2017-11-29 20:27:15 +0800

    Add arg list for hourglass_training_server

--- util/hourglass_training_server/main.c
+++ util/hourglass_training_server/main.c
@@ -1,10 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 #include <tcpmgr.h>
 #include <trasvc.h>
 #include <args.h>
 
-int main()
+char* ts_ip_def[] = {
+	"0.0.0.0"
+};
+
+char* ts_port_def[] = {
+	"7500"
+};
+
+char* ts_max_client_def[] = {
+	"5"
+};
+
+char* ts_dev_timeout_def[] = {
+	"20"
+};
+
+args_t ts_arg_list[] = {
+	{1, "host-ip", 'I', 1, ts_ip_def, "Server Setting", "Host IP dddress"},
+	{1, "host-port", 'P', 1, ts_port_def, "Host port"},
+	{1, "max-client", 'M', 1, ts_max_client_def, "Maximum client connection"},
+	{0, "dev-path", 'D', 1, NULL, "Wheel Device Setting", "Wheel device path"},
+	{0, "dev-baud", 'B', 1, NULL, "Wheel device baudrate"},
+	{1, "timeout", 'T', 1, ts_dev_timeout_def, "Wheel device timeout"},
+	{0, "help", 'H', 0, NULL, "User Interface", "Print detail of arguments"},
+	ARGS_TERMINATE
+};
+
+int main(int argc, char* argv[])
 {
+	int ret;
+
+	char* hostIP;
+	int hostPort, maxClient;
+
+	tcpmgr_t mgr = NULL;
+	trasvc_t ts = NULL;
+
 	return 0;
 }
