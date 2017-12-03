595362b2986229b5e2875d110e18de4e657f289e
Zheng-Ling Lai <jamesljlster@gmail.com>
2017-12-03 21:52:25 +0800

    Add VideoCapture and laneft in ftsvc class

--- lib/ftsvc/ftsvc.cpp
+++ lib/ftsvc/ftsvc.cpp
@@ -3,11 +3,44 @@
 
 #include "debug.h"
 
+using namespace std;
+using namespace cv;
+
 namespace hourglass
 {
 	ftsvc::ftsvc()
 	{
 		this->thStatus = 0;
+	}
+
+	bool ftsvc::open_cam(int camIndex, int width, int height)
+	{
+		bool ret = this->cam.open(camIndex);
+		if(!ret)
+		{
+			return ret;
+		}
+
+		ret = this->cam.set(CV_CAP_PROP_FRAME_WIDTH, width);
+		if(!ret)
+		{
+			this->cam.release();
+			return ret;
+		}
+
+		ret = this->cam.set(CV_CAP_PROP_FRAME_HEIGHT, height);
+		if(!ret)
+		{
+			this->cam.release();
+			return ret;
+		}
+
+		return ret;
+	}
+
+	bool ftsvc::open_cam(string& camPath)
+	{
+		return this->cam.open(camPath);
 	}
 
 	ftsvc::~ftsvc()
@@ -17,5 +50,20 @@ namespace hourglass
 			pthread_cancel(this->th);
 			pthread_join(this->th, NULL);
 		}
+	}
+
+	void ftsvc::set_find_point_rule(int maskSize, int threshold)
+	{
+		this->laneFt.set_find_point_rule(maskSize, threshold);
+	}
+
+	void ftsvc::set_find_line_rule(int maxDist, int threshold)
+	{
+		this->laneFt.set_find_line_rule(maxDist, threshold);
+	}
+
+	void ftsvc::set_line_height_filter(int threshold)
+	{
+		this->laneFt.set_line_height_filter(threshold);
 	}
 }
--- lib/ftsvc/ftsvc.hpp
+++ lib/ftsvc/ftsvc.hpp
@@ -2,6 +2,9 @@
 #define __FTSVC_HPP__
 
 #include <pthread.h>
+#include <string>
+
+#include <opencv2/opencv.hpp>
 
 #include <laneft.hpp>
 
@@ -10,12 +13,27 @@ namespace hourglass
 	class ftsvc
 	{
 		public:
+			// Constructor
 			ftsvc();
+
+			// Destructor
 			~ftsvc();
+
+			// Open cam function
+			bool open_cam(int camIndex, int width, int height);
+			bool open_cam(std::string& camPath);
+
+			// Feature setting
+			void set_find_point_rule(int maskSize, int threshold);
+			void set_find_line_rule(int maxDist, int threshold);
+			void set_line_height_filter(int threshold);
 
 		protected:
 			int thStatus;
 			pthread_t th;
+
+			cv::VideoCapture cam;
+			laneft laneFt;
 	};
 }
 
