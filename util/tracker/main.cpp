4307899ee4599b63d6a6dc2767bd74c587317a2a
Zheng-Ling Lai <jamesljlster@gmail.com>
2017-12-04 14:48:59 +0800

    tracker util init

---
+++ util/tracker/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+#include <ftsvc.hpp>
+#include <wclt.h>
+#include <modcfg.h>
+
+using namespace std;
+using namespace hourglass;
+
+int main(int argc, char* argv[])
+{
+	cout << "Hello" << endl;
+	return 0;
+}
