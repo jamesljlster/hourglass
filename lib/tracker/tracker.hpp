c692278cd94cc5853ca742cb175e8c7e3ed342c1
Zheng-Ling Lai <jamesljlster@gmail.com>
2018-12-22 22:20:12 +0800

    Add ft_type argument and fix comment in default config. Add tracker lib prototype

--- lib/tracker/tracker.hpp
+++ lib/tracker/tracker.hpp
@@ -1,10 +1,51 @@
 #ifndef __TRACKER_HPP__
 #define __TRACKER_HPP__
 
+#include <string>
+
+#include <SPID.h>
+#include <lstm.h>
+#include <trasvc.h>
+#include <ftsvc.hpp>
+#include <wclt.hpp>
+
 class Tracker
 {
    public:
+    // Member variables
+    int stop;
+
+    // Member functions
+    bool init(int argc, char* argv[]);
+    void cleanup();
+
+    bool svc_connect();
+    bool svc_disconnect();
+
    protected:
+    // Member class
+    class TkrNet
+    {
+       public:
+        std::string ip;
+        int port;
+    };
+
+    // Member variables
+    ftsvc ft;
+    fstream fLog;
+
+    TkrNet wsvr;
+    TkrNet trasvr;
+
+    lstm_t model;
+    SPID sPid;
+
+    int wcltStatus;
+    wclt_t wclt;
+
+    int tsStatus;
+    trasvc_client_t ts;
 };
 
 #endif
