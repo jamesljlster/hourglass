4b777ca15e6c0f094ade492663dc92ba8e8754b4
Zheng-Ling Lai <jamesljlster@gmail.com>
2018-12-26 14:38:57 +0800

    Upgrade lstm_tracker program archtecture

--- util/lstm_tracker/main.cpp
+++ util/lstm_tracker/main.cpp
@@ -1,126 +1,51 @@
+#include <exception>
 #include <iostream>
-#include <cstring>
-#include <fstream>
+#include <stdexcept>
 
-#include <ftsvc.hpp>
-
-#include "tracker.hpp"
-
-#define SPEED_MAX 510
-#define SPEED_MIN 0
-
-#define INPUTS 1
-#define OUTPUTS 2
-
-#define CAM_PATH 0
-#define CAM_WIDTH 320
-#define CAM_HEIGHT 180
+#include <tracker.hpp>
 
 using namespace std;
 using namespace hourglass;
 
 int main(int argc, char* argv[])
 {
-	int ret = 0;
-	struct TKR tkr;
-	int sal, sar;
-	ftsvc ft;
-	fstream fLog;
+    int ret = 0;
 
-	float inputList[INPUTS] = {0};
-	float outputList[OUTPUTS] = {0};
+    try
+    {
+        // Initial tracker class
+        Tracker tkr(argc, argv);
 
-	// Initialize
-	if(!tkr_init(&tkr, argc, argv))
-	{
-		cout << "Initialization failed!" << endl;
-		return -1;
-	}
+        // Start log
+        tkr.start_new_log();
 
-	if(lstm_config_get_inputs(lstm_get_config(tkr.model)) != INPUTS ||
-			lstm_config_get_outputs(lstm_get_config(tkr.model)) != OUTPUTS)
-	{
-		cout << "Incompatible LSTM control model!" << endl;
-		goto RET;
-	}
+        // Tracking
+        while (1)
+        {
+            float ctrlErr, sal, sar;
 
-	if(!ft.open_cam(CAM_PATH, CAM_WIDTH, CAM_HEIGHT))
-	{
-		cout << "Failed to open camera!" << endl;
-		goto RET;
-	}
+            // Get feature
+            ctrlErr = tkr.get_norm_feature();
+            if (tkr.ft_kbin() == 27)
+            {
+                throw runtime_error("Program terminated by user");
+            }
 
-	fLog.open(tkr.logPath, ios_base::out);
-	if(!fLog.is_open())
-	{
-		cout << "Failed to open log file with path: " << tkr.logPath << endl;
-		goto RET;
-	}
+            // Find speed
+            tkr.get_norm_ctrl_speed(ctrlErr, &sal, &sar);
 
-	// Setup feature
-	ft.set_line_height_filter(10);
-	ft.set_image_show(1);
+            // Control Wheel
+            tkr.wheel_ctrl(sal, sar);
 
-	// Dump csv header
-	fLog << "error,sal,sar" << endl;
+            // Dump log
+            tkr.dump_info(ctrlErr, sal, sar);
+        }
+    }
+    catch (exception& ex)
+    {
+        cout << "Error occurred: " << ex.what() << endl;
+        ret = -1;
+    }
 
-	// Tracking
-	while(tkr.stop == 0)
-	{
-		// Get feature
-		inputList[0] = ft.get_norm_feature();
-		if(ft.kbin == 27)
-		{
-			tkr.stop = 1;
-		}
-
-		// Find speed
-		lstm_forward_computation(tkr.model, inputList, outputList);
-		sal = outputList[0] * SPEED_MAX;
-		sar = outputList[1] * SPEED_MAX;
-
-		if(sal > SPEED_MAX)
-		{
-			sal = SPEED_MAX;
-		}
-		else if(sal < SPEED_MIN)
-		{
-			sal = SPEED_MIN;
-		}
-
-		if(sar > SPEED_MAX)
-		{
-			sar = SPEED_MAX;
-		}
-		else if(sar < SPEED_MIN)
-		{
-			sar = SPEED_MIN;
-		}
-
-		// Control Wheel
-		ret = wclt_control(tkr.wclt, sal, sar);
-		if(ret < 0)
-		{
-			cout << "wclt_control() failed with error: " << ret << endl;
-			goto RET;
-		}
-
-		// Dump log
-		fLog << inputList[0] << "," << ((float)sal / (float)SPEED_MAX) << "," << ((float)sar / (float)SPEED_MAX) << endl;
-	}
-
-RET:
-	// Stop Wheel
-	ret = wclt_control(tkr.wclt, 255, 255);
-	if(ret < 0)
-	{
-		cout << "wclt_control() failed with error: " << ret << endl;
-		goto RET;
-	}
-
-	// Cleanup
-	fLog.close();
-	tkr_delete(&tkr);
-
-	return ret;
+    return ret;
 }
